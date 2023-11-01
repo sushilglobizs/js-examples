4ed64cf6bbd9d58f91cf2b7f7f4c7988c3117453
Sushil <sushil@globizs.com>
2023-11-01 13:42:01 +0530

    hide show

--- Angular/my-app/src/app/app.component.ts
+++ Angular/my-app/src/app/app.component.ts
@@ -7,30 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  popBands!: string[];
-  rockBands!: string[];
-  hipHopBands!: string[];
-  metalBands!: string[];
-
-  activeMusicType!: string;
-  selectedBand!: string;
+  hide = true;
 
   constructor() {}
 
-  ngOnInit() {
-    this.popBands = ['Adele', 'Lady Gaga', 'Taylor Swift', 'One Direction'];
-    this.rockBands = ['Pink Floyd', 'AC/DC', 'U2', 'Scorpions'];
-    this.hipHopBands = ['Eminem', '50 Cent', 'Snoop Dogg'];
-    this.metalBands = ['Metallica', 'Avenged Sevenfold', 'Megadeth', 'Black Sabbath'];
-  }
+  ngOnInit() {}
 
-  showBands(musicType: string) {
-    // pop | rock | rap | metal
-    this.activeMusicType = musicType;
-  }
-
-  setBand(band: string) {
-    this.selectedBand = band;
+  toggle(visibility: boolean) {
+    this.hide = visibility;
   }
 
 }
