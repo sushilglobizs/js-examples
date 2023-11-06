a7d9391bfb17a67e19bc8d4f5b94dae00d2a8210
Sushil <sushil@globizs.com>
2023-11-06 11:24:44 +0530

    favourites list

--- Angular/my-app/src/app/app.component.ts
+++ Angular/my-app/src/app/app.component.ts
@@ -7,18 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  names!: string[];
+  selectedType!: string;
+
+  favBooks!: string[];
+  favMovies!: string[];
+  favFoods!: string[];
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.favBooks = ['Book 1', 'Book 2', 'Book 3', 'Book 4', 'Book 5', ];
+    this.favMovies = ['Movie 1', 'Movie 2', 'Movie 3', 'Movie 4', 'Movie 5', ];
+    this.favFoods = ['Food 1', 'Food 2', 'Food 3', 'Food 4', 'Food 5', ];
+  }
 
-  suggestNames(g: string) {
-    if (g === 'M') {
-      this.names = ['John', 'Rick', 'Morty'];
-    } else if (g === 'F') {
-      this.names = ['Rosie', 'Stacy', 'Anna'];
-    }
+  choseList(type: string) {
+    this.selectedType = type;
   }
 
 }
